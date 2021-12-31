dea5ee2b2173f49e590a5b21dcbcd65388ef0e6a
Martin Bies <MBies87@googlemail.com>
2021-12-31 08:09:17 +0100

    Use only the parallel root counter

--- counter_H1.cpp
+++ counter_H1.cpp
@@ -22,6 +22,9 @@ boost::mutex myGuard;
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
 
+// Global variables
+int thread_number = 1;
+int display_details = false;
 
 // read out fluxes
 std::vector<std::vector<int>> read_fluxes(const int & file_number, const int & start, const int & end)
@@ -122,7 +125,7 @@ void count_roots(const int & file_number, const int & start, const int & end)
             h0_min = 0;
         }
         for (int j = h0_min; j <= h0Max; j++){
-            dist[j] = NewRootDistributionCounter(degrees, genera, edges, root, graph_stratification, edge_numbers, j);
+            dist[j] = parallel_root_counter(degrees, genera, edges, root, graph_stratification, edge_numbers, j, thread_number, display_details);
         }
         
         // (3.3) remember non-trivial results
